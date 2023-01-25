030dabd05084ac79b0cda11508ca6f931cf70998
gsdutra <gabrielsprecherdutra@gmail.com>
2023-01-25 13:44:44 -0300

    chore: added context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import {useState, useEffect} from 'react'
+import styled from 'styled-components'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import UserContext from './contexts/UserContext.js';
+
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+	const [token, setToken] = useState("")
+
+	return (
+		<div className="App">
+			<UserContext.Provider value={{token, setToken}}>
+				
+			</UserContext.Provider>
+		</div>
+	);
 }
 
 export default App;
