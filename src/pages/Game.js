896d8577cc32a5f13b7569990104a1d708f7d92b
Leonardo Sampaio <leonardo.lps@outlook.com.br>
2023-01-26 19:27:30 -0300

    chore: button home end game

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,51 +1,96 @@
 import styled from "styled-components"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import axios from "axios"
+import { LagContext } from "../contexts/LagContext"
+import { useEffect, useContext, useState } from "react"
+import { useNavigate, Link } from "react-router-dom"
 
 export default function Game(){
+    const { idGame, game, setGame, load } = useContext(LagContext)
+    const navigate = useNavigate()
 
+    useEffect(() => {        
+        const REACT_APP_API_URL = `http://localhost:5000/game/${idGame}` 
+            const url = REACT_APP_API_URL     
+            const promise = axios.get(url) 
+
+            promise.then(res => {  
+                setGame(res.data)           
+            }) 
+
+            promise.catch((err) => {
+                console.log(err)
+            })        
+    }, [])
+
+    function pageHome(){       
+        setGame(undefined)
+        navigate("/home")       
+    }
+
+    if(game === undefined) {
+        return <Load>{load}</Load>;
+    }
+    
     return(
         <>
         <Header/>
-        <ContainerGame>
-            <PainelGame src="https://www.psu.com/wp/wp-content/uploads/2022/06/ResidentEvil4-Remake-PS5-Wallpapers-0.jpg"/>
+        <ContainerGame>        
+            <PainelGame src={game.cape}/>
             <ContainerInfo>
-                <h1>Resident Evil 4</h1>
-                <p>Seis anos se passaram desde o desastre biológico em Raccoon City. O agente Leon S. Kennedy, um dos sobreviventes do incidente, foi enviado para resgatar a filha raptada do presidente. Ele segue o rastro dela até uma isolada vila europeia, onde há algo terrivelmente errado com os habitantes. Então a cortina se abre nessa história de um resgate ousado e um terror medonho, onde vida e morte, terror e catarse, se encontram. Com jogabilidade modernizada, uma história reimaginada e gráficos com detalhes vívidos, Resident Evil 4 marca o renascimento de um gigante da indústria. Reviva o pesadelo que revolucionou o terror de sobrevivência.</p>
-                <button>Adicionar ao Carrinho</button>
-            </ContainerInfo>
+                <h1>{game.title}</h1>
+                <p>{game.description}</p>
+                <ContainerButtons>  
+                    <button onClick={()=>alert("clicou no carrinho")}>Adicionar ao Carrinho</button>
+                    <button onClick={()=>(pageHome())}>Voltar para home</button>                     
+                </ContainerButtons>
+            </ContainerInfo>           
         </ContainerGame>
         <Footer/>
         </>
     )
 }    
 
+const Load = styled.div`
+    display: flex;   
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    padding: 0 20px;
+`
 const ContainerGame = styled.div`
     display: flex; 
     margin: 50px 0 ;
     flex-direction: column;
 `
 const PainelGame = styled.img`
-    width: 100%;
+    //width: 100%;
+    //box-shadow: 0 1px 5px black;
+
+    width: 100%; /* width of container */
+    height: 250px; /* height of container */
+    object-fit: cover;
     box-shadow: 0 1px 5px black;
 `
 const ContainerInfo = styled.div`
-   margin: 20px;
-   h1{
+    margin: 20px;
+    h1{
         font-family: 'Kanit', sans-serif;
         font-style: normal;
         font-weight: 700;
         font-size: 40px;
         color: #FFFFFF;
-   }
-   p{
+    }
+    p{
         font-family: 'Kanit', sans-serif;
         font-style: normal;
         font-size: 16px;
         color: #FFFFFF;
         margin: 10px 0;
-   }
-   button{
+    }
+    button{
         background-color: #4fa94d;
         width: 300px;
         height: 50px;
@@ -57,5 +102,51 @@ const ContainerInfo = styled.div`
         font-size: 20px;
         color: #FFFFFF;
         box-shadow: 0 1px 5px black;
-   }
+    }
+`
+const ContainerButtons = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    button:first-child{
+        background-color: #4fa94d;
+        border: none;
+        border-radius: 50px;
+        width: 300px;
+        height: 50px;
+        font-weight: 700;
+        margin: 5px 5px 10px;
+        font-family: 'Montserrat', sans-serif;
+        font-size: 12px;
+        text-decoration: none;
+        font-family: 'Kanit', sans-serif;
+        font-style: normal;
+        font-size: 20px;
+        color: #FFFFFF;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        box-shadow: 0 1px 5px black;
+    }
+    button:last-child{
+        background-color: #db3636;
+        border: none;
+        border-radius: 50px;
+        width: 300px;
+        height: 50px;
+        font-weight: 700;
+        margin: 5px 5px 10px;
+        font-family: 'Montserrat', sans-serif;
+        font-size: 12px;
+        text-decoration: none;
+        padding: 10px;
+        font-family: 'Kanit', sans-serif;
+        font-style: normal;
+        font-size: 20px;
+        color: #FFFFFF;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        box-shadow: 0 1px 5px black;
+    }
 `
